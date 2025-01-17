248cf42cdaf7da7c04d9a45aa8fd4cf23f7d42b0
lydia <lydia.kidwell22@gmail.com>
2025-01-17 10:03:53 -0500

    continuation of PortfolioWebsite repo. Add projects on projects page with projects database, add edit project, view singular project, and add project, edit about section, edit main.py to run on pythonanywhere and create a copy of main.py (main_local) to test locally

---
+++ main.py
@@ -0,0 +1,113 @@
+import smtplib
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, send_from_directory
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from socket import gethostname
+
+# Optional: add contact me email functionality (Day 60)
+import smtplib
+
+'''
+Make sure the required packages are installed: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from the requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Configure Project Table
+class Projects(db.Model):
+    __tablename__ = "projects"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    file: Mapped[str] = mapped_column(String(250), nullable=False)
+    github_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", status="home")
+
+
+@app.route("/about")
+def about():
+    with open("about.txt", mode="r") as about_file:
+        about_text = about_file.read()
+    return render_template("about.html", status="about", about_text=about_text)
+
+
+@app.route("/resume", methods=["GET", "POST"])
+def resume():
+    return render_template("resume.html", status="resume")
+
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', path="files/LydiaKidwell_Resume_2025.pdf")
+
+
+@app.route('/projects')
+def projects():
+    result = db.session.execute(db.select(Projects))
+    all_projects = result.scalars().all()
+    return render_template("projects.html", all_projects=all_projects, status="projects")
+
+
+MAIL_ADDRESS = os.environ.get("MY_EMAIL")
+MAIL_APP_PW = os.environ.get("MY_EMAIL_PASS")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True, status="contact")
+    return render_template("contact.html", msg_sent=False, status="contact")
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MAIL_ADDRESS, MAIL_APP_PW)
+        connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
+
+
+if __name__ == "__main__":
+    db.create_all()
+    if 'liveconsole' not in gethostname():
+        app.run(debug=True, port=5001)
---
+++ main_local.py
@@ -0,0 +1,168 @@
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, send_from_directory
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.fields.choices import SelectField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+from socket import gethostname
+
+# Optional: add contact me email functionality (Day 60)
+import smtplib
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Configure Project Table
+class Projects(db.Model):
+    __tablename__ = "projects"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    category: Mapped[str] = mapped_column(String(30), nullable=False)
+    date_finished: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    file: Mapped[str] = mapped_column(String(250), nullable=True)
+    github_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# WTForm for creating a blog post
+class CreateProjectForm(FlaskForm):
+    title = StringField("Project Title", validators=[DataRequired()])
+    category = SelectField("Type of Project", choices=["Data Analysis", "Web Design & Development", "Game Design", "Other"], validators=[DataRequired()])
+    date_finished = StringField("Date Completed (mm-yyyy)")
+    description = CKEditorField("Project Description", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    file = StringField("File Path")
+    github_url = StringField("Github URL")
+    submit = SubmitField("Submit Project")
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", status="home")
+
+
+@app.route("/about")
+def about():
+    with open("about.txt", mode="r") as about_file:
+        #create a list of paragraphs for the about section
+        about_text = about_file.read().split("CHUNK")
+    return render_template("about.html", status="about", about_text=about_text)
+
+
+@app.route("/resume", methods=["GET", "POST"])
+def resume():
+    return render_template("resume.html", status="resume")
+
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', path="files/LydiaKidwell_Resume_2025.pdf")
+
+
+@app.route('/projects')
+def projects():
+    result = db.session.execute(db.select(Projects))
+    all_projects = result.scalars().all()
+    return render_template("all_projects.html", all_projects=all_projects, status="projects")
+
+
+@app.route(f"/{os.environ.get('SECRET_URL')}", methods=["GET", "POST"])
+def add_project():
+    form = CreateProjectForm()
+    if form.validate_on_submit():
+        new_post = Projects(
+            title=form.title.data,
+            category=form.category.data,
+            date_finished=form.date_finished.data,
+            description=form.description.data,
+            img_url=form.img_url.data,
+            github_url=form.github_url.data
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("projects"))
+    return render_template("add_project.html", form=form)\
+
+
+@app.route("/edit-project-<int:project_id>", methods=["GET", "POST"])
+def edit_project(project_id):
+    project = db.get_or_404(Projects, project_id)
+    edit_form = CreateProjectForm(
+        title=project.title,
+        category=project.category,
+        date_finished=project.date_finished,
+        description=project.description,
+        img_url=project.img_url,
+        github_url=project.github_url
+    )
+    if edit_form.validate_on_submit():
+        project.title = edit_form.title.data
+        project.category = edit_form.category.data
+        project.date_finished = edit_form.date_finished.data
+        project.description = edit_form.description.data
+        project.img_url = edit_form.img_url.data
+        project.github_url = edit_form.github_url.data
+
+        db.session.commit()
+
+        return redirect(url_for("show_project", project_id=project.id, status="projects"))
+
+    return render_template("add_project.html", form=edit_form, is_edit=True, status="projects")
+
+# Add a POST method to be able to post comments
+@app.route("/project-<int:project_id>", methods=["GET", "POST"])
+def show_project(project_id):
+    requested_project = db.get_or_404(entity=Projects, ident=project_id, description="This project does not exist.")
+
+    return render_template("project.html", project=requested_project, status="projects")
+
+
+MAIL_ADDRESS = os.environ.get("MY_EMAIL")
+MAIL_APP_PW = os.environ.get("MY_EMAIL_PASS")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True, status="contact")
+    return render_template("contact.html", msg_sent=False, status="contact")
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MAIL_ADDRESS, MAIL_APP_PW)
+        connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
+
+
+if __name__ == "__main__":
+    print(os.environ.get('SECRET_URL'))
+    app.run(debug=True, port=5001)
+    print(os.environ.get('SECRET_URL'))
