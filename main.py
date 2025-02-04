97220867d8e5f88ec0444c8e5ee1147601ceef9c
lydia <lydia.kidwell22@gmail.com>
2025-02-04 10:11:53 -0500

    update main with main_local

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
-from flask import Flask, render_template, redirect, url_for, request, send_from_directory
+from functools import wraps
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory, session, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor, CKEditorField
 from flask_sqlalchemy import SQLAlchemy
@@ -9,7 +10,7 @@ import os
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.fields.choices import SelectField
-from wtforms.fields.simple import URLField
+from wtforms.fields.simple import EmailField, URLField
 from wtforms.validators import DataRequired, URL
 from socket import gethostname
 from dotenv import load_dotenv
@@ -26,7 +27,7 @@ class Base(DeclarativeBase):
 	pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///projects.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -36,6 +37,7 @@ class Projects(db.Model):
 	__tablename__ = "projects"
 	id: Mapped[int] = mapped_column(Integer, primary_key=True)
 	title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+	subtitle: Mapped[str] = mapped_column(String(50), nullable=True)
 	category: Mapped[str] = mapped_column(String(30), nullable=False)
 	date_finished: Mapped[str] = mapped_column(String(250), nullable=False)
 	description: Mapped[str] = mapped_column(Text, nullable=False)
@@ -44,9 +46,14 @@ class Projects(db.Model):
 	github_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
+with app.app_context():
+	db.create_all()
+
+
 # WTForm for creating a blog post
 class CreateProjectForm(FlaskForm):
 	title = StringField("Project Title", validators=[DataRequired()])
+	subtitle = StringField("Project Subtitle")
 	category = SelectField("Type of Project",
 						   choices=["Data Analysis", "Web Design & Development", "Game Design", "Other"],
 						   validators=[DataRequired()]
@@ -57,6 +64,30 @@ class CreateProjectForm(FlaskForm):
 	img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
 	github_url = URLField("Github URL")
 	submit = SubmitField("Submit Project")
+
+
+# WTForm for contact page
+class CreateContactForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = EmailField("Email Address", validators=[DataRequired()])
+	phone = StringField("Phone Number", validators=[DataRequired()])
+	message = CKEditorField("Message", validators=[DataRequired()])
+	submit = SubmitField("Submit Message")
+
+
+site_password = os.getenv('SECRET_URL')
+
+
+# custom decorator to check password
+def check_pw(func):
+	@wraps(func)
+	def decorated_function(*args, **kwargs):
+		status = session.get('status')
+		if status != "good":
+			return redirect(url_for('login'))
+		return func(*args, **kwargs)
+
+	return decorated_function
 
 
 @app.route('/')
@@ -91,29 +122,33 @@ def projects():
 
 # @app.route(f"/{os.environ.get('SECRET_URL')}", methods=["GET", "POST"])
 @app.route("/add-project", methods=["GET", "POST"])
+@check_pw
 def add_project():
 	form = CreateProjectForm()
 	if form.validate_on_submit():
-		new_post = Projects(
+		new_project = Projects(
 				title=form.title.data,
+				subtitle=form.subtitle.data,
 				category=form.category.data,
 				date_finished=form.date_finished.data,
-				description=form.description.data.encode(),
+				description=form.description.data,
 				tags=form.tags.data,
 				img_url=form.img_url.data,
 				github_url=form.github_url.data
 				)
-		db.session.add(new_post)
+		db.session.add(new_project)
 		db.session.commit()
-		return redirect(url_for("projects"))
+		return redirect(url_for("show_project", project_id=new_project.id))
 	return render_template("add_project.html", form=form)
 
 
 @app.route("/edit-project-<int:project_id>", methods=["GET", "POST"])
+@check_pw
 def edit_project(project_id):
 	project = db.get_or_404(Projects, project_id)
 	edit_form = CreateProjectForm(
 			title=project.title,
+			subtitle=project.subtitle,
 			category=project.category,
 			date_finished=project.date_finished,
 			description=project.description,
@@ -123,6 +158,7 @@ def edit_project(project_id):
 			)
 	if edit_form.validate_on_submit():
 		project.title = edit_form.title.data
+		project.subtitle = edit_form.subtitle.data
 		project.category = edit_form.category.data
 		project.date_finished = edit_form.date_finished.data
 		project.description = edit_form.description.data
@@ -151,11 +187,11 @@ MAIL_APP_PW = os.environ.get("EMAIL_APP_PASS")
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-	if request.method == "POST":
-		data = request.form
-		send_email(data["name"], data["email"], data["phone"], data["message"])
-		return render_template("contact.html", msg_sent=True, status="contact")
-	return render_template("contact.html", msg_sent=False, status="contact")
+	form = CreateContactForm()
+	if form.validate_on_submit():
+		send_email(form.name.data, form.email.data, form.phone.data, form.message.data)
+		return redirect(url_for("contact", msg_sent=True, status="contact"))
+	return render_template("contact_form.html", form=form, msg_sent=False, status="contact")
 
 
 def send_email(name, email, phone, message):
@@ -166,10 +202,21 @@ def send_email(name, email, phone, message):
 		connection.sendmail(from_addr=email, to_addrs=MAIL_ADDRESS, msg=email_message)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == "POST":
+		if request.form.get("password") != site_password:
+			flash('wrong password! try again...')
+			return redirect(request.url)
+		else:
+			session["status"] = 'good'
+			return redirect(url_for("projects"))
+	return render_template('login.html')
+
+
 @app.context_processor
 def add_year():
 	return {"current_year": str(datetime.datetime.now().year)}
-
 
 if __name__ == "__main__":
 	db.create_all()
